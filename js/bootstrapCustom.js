e1da94c4d76ca250ceb89981f26e77a141ab26a9
siouxmcd <claeomcdermott@u.boisestate.edu>
2017-04-05 14:36:38 -0600

    click div do something

--- js/bootstrapCustom.js
+++ js/bootstrapCustom.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+/*window.onload = function(){
 	var itemClick = document.getElementsByClassName("storeItem");
 	itemClick.onclick = iClick;
 };
@@ -6,4 +6,10 @@ window.onload = function(){
 function iClick(){
 	var curImage = document.getElementById("itemImg");
 	curImage.style.width = 250;
-}
+}*/
+
+$('.storeItem').click(function(){
+	for (var i=0; i<=1; i++){
+	document.getElementsByClassName('storeItem')[i].style.margin = '10px';
+	}
+});
