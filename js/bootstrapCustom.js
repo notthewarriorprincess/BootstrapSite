37a6157adfb1a005c743d057db769908b053a657
siouxmcd <claeomcdermott@u.boisestate.edu>
2017-04-06 00:01:28 -0600

    specific store items do things now

--- js/bootstrapCustom.js
+++ js/bootstrapCustom.js
@@ -8,8 +8,20 @@ function iClick(){
 	curImage.style.width = 250;
 }*/
 
-$('.storeItem').click(function(){
+/*$('.storeItem').click(function(){
 	for (var i=0; i<=1; i++){
 	document.getElementsByClassName('storeItem')[i].style.margin = '10px';
 	}
-});
+});*/
+
+function itemSelected(id){
+	var item = document.getElementById(id);
+	var itemdiv = document.getElementById('shoes').style.height = '250px';
+	if (item.style.width != '250px'){
+		item.style.width = '250px';
+		itemdiv.style.height = '270px';
+	}else{
+		item.style.width = '150px';
+		itemdiv.style.height = '170px';
+	}
+}
